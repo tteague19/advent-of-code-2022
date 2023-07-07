5d48b4ea0d28ab558a1e55f2f8c42d180e134474
Thomas Teague <24764359+tteague19@users.noreply.github.com>
2023-07-07 09:48:59 -0400

    Write a test for the compute_score method
    This change creates a test that will pass if the compute_score method
    of Round is implemented correctly.

--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -1,9 +1,11 @@
 """Tests the solution of the challenge for Day 2."""
+from itertools import repeat
 from pathlib import Path
 
 import pytest
 
-from days.day_02 import Round, OpponentChoice, PlayerChoice, RoundOutcome
+from days.day_02 import Round, OpponentChoice, PlayerChoice, RoundOutcome, \
+    RoundScore, PLAYER_SELECTION_TO_SCORE_DICT, SelectionScore
 
 INPUT_FILE_PATH = Path(__file__).parent.joinpath(
     "input-files", "day-02-simple.txt")
@@ -112,3 +114,35 @@ def test_round_determine_outcome(
     """
     actual_outcome = round_obj.determine_outcome()
     assert actual_outcome == expected_outcome
+
+
+@pytest.mark.parametrize(
+    "input_line,expected_score,selection_score_dict",
+    zip(
+        INPUT_FILE_PATH.open(mode="r").readlines(),
+        [8, 1, 6],
+        repeat(PLAYER_SELECTION_TO_SCORE_DICT, 3),
+    )
+)
+def test_round_compute_score(
+        input_line: str,
+        expected_score: RoundScore,
+        selection_score_dict: dict[PlayerChoice, SelectionScore],
+) -> None:
+    """
+    Compare an expected score for each round to the output from a method.
+
+    :param input_line: A line from a file that defines a move by an opponent
+        followed by a move from a player
+    :type input_line: str
+    :param expected_score: The score we expect the compute_score method to
+        return for each round
+    :type expected_score: RoundScore
+    :param selection_score_dict: A dictionary that maps a player choice to a
+        score associated with that choice
+    :type selection_score_dict: dict[PlayerChoice, SelectionScore]
+    """
+    round_obj = Round.from_string(input_string=input_line, split_string=" ")
+    actual_score = round_obj.compute_score(
+        selection_score_dict=selection_score_dict)
+    assert actual_score == expected_score
