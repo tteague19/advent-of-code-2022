de43675d5fd415f5a137fb51eed69da856640543
Thomas Teague <24764359+tteague19@users.noreply.github.com>
2023-07-07 09:36:08 -0400

    Implement test for determining outcome of a round
    This change creates a test to verify that the determine_outcome method
    of a Round object correctly determines the outcome of a game of Rock
    Paper Scissors from the perspective of the player.

--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pytest
 
-from days.day_02 import Round, OpponentChoice, PlayerChoice
+from days.day_02 import Round, OpponentChoice, PlayerChoice, RoundOutcome
 
 INPUT_FILE_PATH = Path(__file__).parent.joinpath(
     "input-files", "day-02-simple.txt")
@@ -42,3 +42,73 @@ def test_round_from_string(input_line: str, expected_round: Round) -> None:
     """
     actual_round = Round.from_string(input_string=input_line, split_string=" ")
     assert actual_round == expected_round
+
+
+@pytest.mark.parametrize(
+    "round_obj,expected_outcome",
+    zip(
+        [
+            Round(
+                opponent_choice=OpponentChoice.ROCK,
+                player_choice=PlayerChoice.ROCK,
+            ),
+            Round(
+                opponent_choice=OpponentChoice.SCISSORS,
+                player_choice=PlayerChoice.SCISSORS,
+            ),
+            Round(
+                opponent_choice=OpponentChoice.PAPER,
+                player_choice=PlayerChoice.PAPER,
+            ),
+            Round(
+                opponent_choice=OpponentChoice.SCISSORS,
+                player_choice=PlayerChoice.ROCK,
+            ),
+            Round(
+                opponent_choice=OpponentChoice.ROCK,
+                player_choice=PlayerChoice.PAPER,
+            ),
+            Round(
+                opponent_choice=OpponentChoice.PAPER,
+                player_choice=PlayerChoice.SCISSORS,
+            ),
+            Round(
+                opponent_choice=OpponentChoice.ROCK,
+                player_choice=PlayerChoice.SCISSORS,
+            ),
+            Round(
+                opponent_choice=OpponentChoice.PAPER,
+                player_choice=PlayerChoice.ROCK,
+            ),
+            Round(
+                opponent_choice=OpponentChoice.SCISSORS,
+                player_choice=PlayerChoice.PAPER,
+            ),
+        ],
+        [
+            RoundOutcome.DRAW,
+            RoundOutcome.DRAW,
+            RoundOutcome.DRAW,
+            RoundOutcome.WIN,
+            RoundOutcome.WIN,
+            RoundOutcome.WIN,
+            RoundOutcome.LOSS,
+            RoundOutcome.LOSS,
+            RoundOutcome.LOSS,
+        ]
+    )
+)
+def test_round_determine_outcome(
+        round_obj: Round, expected_outcome: RoundOutcome) -> None:
+    """
+    Verify determine_outcome method of Round computes the appropriate outcome.
+
+    :param round_obj: An object that represents a round of play in a game of
+        Rock Paper Scissors
+    :type round_obj: Round
+    :param expected_outcome: The outcome we expect the :param:`round_obj`
+        to produce
+    :type expected_outcome: RoundOutcome
+    """
+    actual_outcome = round_obj.determine_outcome()
+    assert actual_outcome == expected_outcome
